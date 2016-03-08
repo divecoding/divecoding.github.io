de5f3d6393070d48b8a512e8180f4ebb38ce713e
joshfermin <joshfermin@gmail.com>
2016-03-08 14:56:31 -0700

    Added sweet alert to finish tutorial loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
 jQuery(document).ready(function($){
+  var search      = window.location.search;  // Returns path only
+  if(search.indexOf("app=true") > -1){
+    swal("Good job, and thank you!", "You completed the trial, if you enjoyed this and want to be updated on our product, sign up today!", "success")
+  }
+
   var isLateralNavAnimating = false;
 
   $('.menu-burger').on('click', function(e) {
